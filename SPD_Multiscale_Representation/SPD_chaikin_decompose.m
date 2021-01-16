ac5be146442018270d1b87a52a82789987fc072e
Wael Mattar <waelmattar.95@gmail.com>
2021-01-16 14:49:29 +0200

    corner-cutting details fixed

--- SPD_Multiscale_Representation/SPD_chaikin_decompose.m
+++ SPD_Multiscale_Representation/SPD_chaikin_decompose.m
@@ -4,9 +4,13 @@ prev_data  = SPD_chaikin_decimate(data);
 
 refined   = SPD_chaikin_refine(prev_data);
 
-for k = 1 : length(data)
-    details{k}  =  SPD_Log(refined{k},data{k});
+for k = 1 : length(data)-3
+    details{k}  =  SPD_Log(refined{k},data{k+2});
 end
+
+details{length(data)-2} =  details{1};
+details{length(data)-1} =  details{2};
+details{length(data)} =  details{3};
 
 end
 
