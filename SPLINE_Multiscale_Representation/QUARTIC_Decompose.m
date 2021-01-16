36afd1b4598eee1184bd8f52224875056df80c25
Wael Mattar <waelmattar.95@gmail.com>
2021-01-16 14:47:08 +0200

    details calculation fixed

--- SPLINE_Multiscale_Representation/QUARTIC_Decompose.m
+++ SPLINE_Multiscale_Representation/QUARTIC_Decompose.m
@@ -7,9 +7,10 @@ refined    =  QUARTIC_refine(prev_data);
 
 details    =  zeros(length(data), 1);
 
-for k = 1 : length(data)
-    details(k)  =  data(k) - refined(k);
+for k = 1 : length(data)-14
+    details(k)  =  data(k+13) - refined(k);
 end
 
+details(end-13: end) =  details(1:14);
 
 end
